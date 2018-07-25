280840296c0f6fdf08c7e15f3b83175afeec8921
RalfG <ralfg@hotmail.be>
2018-07-25 14:14:36 +0200

    setup.py now takes model argument So now only one setup.py is needed for all models

--- compile.sh
+++ compile.sh
@@ -1,17 +1,20 @@
 rm -f ms2pipfeatures_pyx_CID.c ms2pipfeatures_pyx_CID.so
-python3 setup_CID.py build_ext --inplace
+python3 setup.py build_ext --inplace CID
 
 rm -f ms2pipfeatures_pyx_HCD.c ms2pipfeatures_pyx_HCD.so
-python3 setup_HCD.py build_ext --inplace
+python3 setup.py build_ext --inplace HCD
 
 #rm -f ms2pipfeatures_pyx_HCDiTRAQ4.c ms2pipfeatures_pyx_HCDiTRAQ4.so
-#python3 setup_HCDiTRAQ.py build_ext --inplace
+#python3 setup.py build_ext --inplace HCDiTRAQ
 
 #rm -f ms2pipfeatures_pyx_HCDiTRAQ4phospho.c ms2pipfeatures_pyx_HCDiTRAQ4phospho.so
-#python3 setup_HCDiTRAQphospho.py build_ext --inplace
+#python3 setup.py build_ext --inplace HCDiTRAQphospho
 
 rm -f ms2pipfeatures_pyx_ETD.c ms2pipfeatures_pyx_ETD.so
-python3 setup_ETD.py build_ext --inplace
+python3 setup.py build_ext --inplace ETD
 
 rm -f ms2pipfeatures_pyx_HCDch2.c ms2pipfeatures_pyx_HCDch2.so
-python3 setup_HCDch2.py build_ext --inplace
+python3 setup.py build_ext --inplace HCDch2
+
+rm -f ms2pipfeatures_pyx_HCDphospho.c ms2pipfeatures_pyx_HCDphospho.so
+python3 setup.py build_ext --inplace HCDphospho
